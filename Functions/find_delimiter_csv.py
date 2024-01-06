245889cc039aec21d2d8203b619aee3a07c18e15
Sarah Morard <153022338+sarahmariamorard@users.noreply.github.com>
2024-01-06 18:10:10 +0100

    Writing functions

---
+++ Functions/find_delimiter_csv.py
@@ -0,0 +1,18 @@
+#Updated on 06.01.2024
+
+def find_delimiter_csv( filename ) :
+    #Goal: Find the seperator in a csv file automatically
+    
+    ## Packages
+    import csv  #The csv module implements classes to read and write tabular data 
+                #in CSV format. 
+    
+    sniffer = csv.Sniffer()  #The Sniffer class is used to deduce the format of a CSV file.
+    
+    with open( filename ) as fp : #It opens each line of the csv file
+        delimiter = sniffer.sniff( fp.read( 5000 ) ).delimiter  #Analyze the given sample
+                                                                #It finds the separator.
+    return delimiter
+
+
+
